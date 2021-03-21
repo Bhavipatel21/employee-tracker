edebbab2f5f1dd366f2a18145106218aa6b12442
Bhavika Patel <bhavij28@gmail.com>
2021-03-20 23:12:34 -0400

    Added function to add employee

--- db/employee-Schema.sql
+++ db/employee-Schema.sql
@@ -13,20 +13,47 @@ CREATE TABLE Role (
     role_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL(10,4),
-    department_id INT NOT NULL,
+    dept_id INT NOT NULL,
     PRIMARY KEY(role_id),
-    FOREIGN KEY (department_id) REFERENCES department (dept_id)
+    FOREIGN KEY (dept_id) REFERENCES department (dept_id) 
 );
 
 CREATE TABLE Employee (
     emp_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT NULL,
+    role_id INT NOT NULL,
     manager_id INT NULL,
     PRIMARY KEY(emp_id),
     FOREIGN KEY (role_id) REFERENCES role (role_id),
     FOREIGN KEY (manager_id) REFERENCES employee (emp_id) 
 
 );
+--INSERT data into Department table--
+INSERT INTO Department (dept_name)
+VALUE ("IT"),("Operations"),("Accounting"),("Retail");
 
+--INSERT data into Role table--
+INSERT INTO Role (title, salary, dept_id)
+VALUES 
+("QA Analyst", 150000, 1),
+("Business Analyst", 125000, 1),
+("Operation Lead", 100000, 2),
+("Supply Chain Specialist", 80000, 2),
+("CPA", 120000, 3),
+("Accounting Manager", 190000, 3),
+("Software Engineer", 120000, 3),
+("Cashier", 90000, 4),
+("Sales Associate", 100000, 4);
+
+--INSERT data into Role table--
+INSERT INTO Employee (first_name, last_name, manager_id, role_id)
+VALUES 
+("Ted", "Mosby", null, 1),
+("Marshall", "Eriksen", null, 2),
+("Robin","Scherbatsky",null,3),
+("Barney", "Stinson", 1, 4),
+("Lily", "Aldrin", 4, 5),
+("Tracy", "McConnell", 1, 6),
+("Kevin", "Penn", 2, 8);
+("Bob", "Saget", 2, 9);
--- employeetracker.js
+++ employeetracker.js
@@ -1,5 +1,8 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const figlet = require('figlet');
+require('console.table');
+
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -11,12 +14,13 @@ const connection = mysql.createConnection({
   user: 'root',
 
   // Be sure to update with your own MySQL password!
-  password: 'Warof1812$&git ',
+  password: 'Warof1812$&',
   database: 'employee_DB',
 });
 
 connection.connect((err) => {
   if (err) throw err;
+  console.log(figlet.textSync('Employee Tracker'));
   runSearch();
 });
 
@@ -29,47 +33,155 @@ const runSearch = () => {
       choices: [
         'View All Employees',
         'View All Employees By Department',
-        'View All Employees By Manager',
         'Add an Employee',
         'Remove an Employee',
         'Update an Employee Role',
         'Update an Employee Manager'
-],
+      ],
     })
     .then((answer) => {
-    switch (answer.action) {
+      switch (answer.action) {
 
         case 'View All Employees':
-        viewAllEmployee();
-        break;
+          viewAllEmployee();
+          break;
 
         case 'View All Employees By Department':
-        viewEmpByDept();
-        break;
-
-        case 'View All Employees By Manager':
-        viewEmpByMngr();
-        break;
+          viewEmpByDept();
+          break;
 
         case 'Add an Employee':
-        addEmp();
-        break;
+          addEmp();
+          break;
 
         case 'Remove an Employee':
-        removeEmp();
-        break;
-        
+          removeEmp();
+          break;
+
         case 'Update an Employee Role':
-        updateEmpRole();
-        break;
+          updateEmpRole();
+          break;
 
         case 'Update an Employee Manager':
-        udpateEmpMngr();
-        break;
+          udpateEmpMngr();
+          break;
 
         default:
-        break;
-    }
+          break;
+      }
     });
+  //View All Employee
+  const viewAllEmployee = () => {
+
+    const query = `SELECT 
+          e.emp_id,
+          e.first_name,
+          e.last_name,
+          e.role_id,
+          e.manager_id,
+          r.role_id,
+          r.title,
+          r.salary,
+          r.dept_id,
+          d.dept_id,
+          d.dept_name
+          FROM employee_db.Employee e
+          left join role r on e.role_id = r.role_id
+          left join department d on r.dept_id = d.dept_id`
+
+    connection.query(query, (err, res) => {
+      console.table(res);
+      runSearch();
+    });
+  };
+  //Vew Employee By Department 
+  const viewEmpByDept = () => {
+
+    const query = `SELECT 
+    concat( e.first_name, ',', e.last_name) as EmployeeFullName,
+    d.dept_name as DepartmentName
+    FROM employee_db.Employee e
+    left join role r on e.role_id = r.role_id
+    left join department d on r.dept_id = d.dept_id
+    order by d.dept_name`
+
+    connection.query(query, (err, res) => {
+      console.table(res);
+      runSearch();
+    });
+  };
+
+  //function to add new employee
+  const addEmp = () => {
+
+    connection.query('SELECT role_id, title, salary,dept_id FROM role', (err, res) => {
+      if (err) throw err;
+
+      inquirer.prompt([
+        {
+          name: 'first_name',
+          type: 'input',
+          message: 'Please enter the first name of the new employee: ',
+          validate: function validateInput(name) {
+            return name !== '';
+          }
+        },
+        {
+          name: 'last_name',
+          type: 'input',
+          message: 'Please enter the last name of the new employee: ',
+          validate: function validateInput(name) {
+            return name !== '';
+          }
+        },
+        {
+          name: 'choose_role',
+          type: 'rawlist',
+          choices() {
+            const choices = [];
+            res.forEach(({ title }) => {
+              choices.push(title);
+            })
+            return choices;
+          },
+          message: 'Choose role for new employee: ',
+        }
+      ])
+        .then((data) => {
+          let roleID;
+          res.forEach((res) => {
+            if (res.title === data.choose_role) {
+              roleID = res.dept_id;
+            }
+          })
+
+          connection.query(
+            'INSERT INTO employee SET ?',
+            {
+              first_name: data.first_name,
+              last_name: data.last_name,
+              role_id: roleID
+            },
+            (err) => {
+              if (err) throw err;
+
+              console.log(`New Employee ${data.first_name} ${data.last_name} added!`);
+
+              runSearch();
+            })
+
+        })
+
+    });
+  }
+
+
+
+  
+
 };
+
+
+
+
 
