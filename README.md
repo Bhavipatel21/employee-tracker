ed23dab150203ce5b97e4caae75814c344cf8c90
Bhavika Patel <bhavij28@gmail.com>
2021-03-21 22:38:28 -0400

    added function to updated employee role and readme

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# employee-tracker
+## Project Title
+  #Employee Tracker
+  
+
+## Description
+
+A Node.js application which allows users to view, add and update employee information to database.
+
+## Installation Instructions
+
+You will need node.js installed 
+* [Node.js](https://en.wikipedia.org/wiki/Node.js)
+
+* [figlet](https://www.npmjs.com/package/figlet)
+
+* Use the [MySQL](https://www.npmjs.com/package/mysql) NPM package to connect to your MySQL database and perform queries.
+
+* Use [InquirerJs](https://www.npmjs.com/package/inquirer/v/0.2.3) NPM package to interact with the user via the command-line.
+
+* Use [console.table](https://www.npmjs.com/package/console.table) to print MySQL rows to the console. There is a built-in version of `console.table`, but the NPM package formats the data a little better for our purposes.
+
+
+To install the required dependencies, please run the following command:
+
+```
+npm init -y
+npm i mysql
+npm i inquirer
+npm i console.table
+npm i figlet
+
+```
+
+## Usage
+Walthrough of for Employee Tracker
+
+[Video]()
+
+## Mock-up
+
+
+## Contribution Guidelines
+Link to Contributor's Covenant: https://www.contributor-covenant.org/version/2/0/code_of_conduct/
+
+## License
+![badge](https://img.shields.io/badge/license-MIT-blueviolet)
+
+*This project uses the [MIT](https://spdx.org/licenses/MIT.html) license.*
+
+
+## Questions
+Github Profile : [Creator]:(https://github.com/bhavipatel21)
+
+If you have any questions please email me at bhavij0511@gmail.com
+  
+  
--- employeetracker.js
+++ employeetracker.js
@@ -1,69 +1,56 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
-const figlet = require('figlet');
-require('console.table');
-
-
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const figlet = require("figlet");
+require("console.table");
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
 
   // Your port; if not 3306
   port: 3306,
 
   // Your username
-  user: 'root',
+  user: "root",
 
   // Be sure to update with your own MySQL password!
-  password: 'Warof1812$&',
-  database: 'employee_DB',
+  password: "Warof1812$&",
+  database: "employee_DB",
 });
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log(figlet.textSync('Employee Tracker'));
+  console.log(figlet.textSync("Employee Tracker"));
   runSearch();
 });
 
 const runSearch = () => {
   inquirer
     .prompt({
-      name: 'action',
-      type: 'rawlist',
-      message: 'What would you like to do?',
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
       choices: [
-        'View All Employees',
-        'View All Employees By Department',
-        'Add an Employee',
-        'Remove an Employee',
-        'Update an Employee Role',
-        'Update an Employee Manager'
+        "View All Employees",
+        "View All Employees By Department",
+        "Add an Employee",
+        "Update an Employee Role",
       ],
     })
     .then((answer) => {
       switch (answer.action) {
-
-        case 'View All Employees':
+        case "View All Employees":
           viewAllEmployee();
           break;
 
-        case 'View All Employees By Department':
+        case "View All Employees By Department":
           viewEmpByDept();
           break;
 
-        case 'Add an Employee':
-          addEmp();
+        case "Add an Employee":
+          addEmployee();
           break;
 
-        case 'Remove an Employee':
-          removeEmp();
-          break;
-
-        case 'Update an Employee Role':
+        case "Update an Employee Role":
           updateEmpRole();
-          break;
-
-        case 'Update an Employee Manager':
-          udpateEmpMngr();
           break;
 
         default:
@@ -72,7 +59,6 @@ const runSearch = () => {
     });
   //View All Employee
   const viewAllEmployee = () => {
-
     const query = `SELECT 
           e.emp_id,
           e.first_name,
@@ -87,23 +73,22 @@ const runSearch = () => {
           d.dept_name
           FROM employee_db.Employee e
           left join role r on e.role_id = r.role_id
-          left join department d on r.dept_id = d.dept_id`
+          left join department d on r.dept_id = d.dept_id`;
 
     connection.query(query, (err, res) => {
       console.table(res);
       runSearch();
     });
   };
-  //Vew Employee By Department 
+  //Vew Employee By Department
   const viewEmpByDept = () => {
-
     const query = `SELECT 
     concat( e.first_name, ',', e.last_name) as EmployeeFullName,
     d.dept_name as DepartmentName
     FROM employee_db.Employee e
     left join role r on e.role_id = r.role_id
     left join department d on r.dept_id = d.dept_id
-    order by d.dept_name`
+    order by d.dept_name`;
 
     connection.query(query, (err, res) => {
       console.table(res);
@@ -112,76 +97,127 @@ const runSearch = () => {
   };
 
   //function to add new employee
-  const addEmp = () => {
+  const addEmployee = () => {
+    connection.query(
+      "SELECT role_id, title, salary,dept_id FROM role",
+      (err, res) => {
+        if (err) throw err;
 
-    connection.query('SELECT role_id, title, salary,dept_id FROM role', (err, res) => {
-      if (err) throw err;
-
-      inquirer.prompt([
-        {
-          name: 'first_name',
-          type: 'input',
-          message: 'Please enter the first name of the new employee: ',
-          validate: function validateInput(name) {
-            return name !== '';
-          }
-        },
-        {
-          name: 'last_name',
-          type: 'input',
-          message: 'Please enter the last name of the new employee: ',
-          validate: function validateInput(name) {
-            return name !== '';
-          }
-        },
-        {
-          name: 'choose_role',
-          type: 'rawlist',
-          choices() {
-            const choices = [];
-            res.forEach(({ title }) => {
-              choices.push(title);
-            })
-            return choices;
-          },
-          message: 'Choose role for new employee: ',
-        }
-      ])
-        .then((data) => {
-          let roleID;
-          res.forEach((res) => {
-            if (res.title === data.choose_role) {
-              roleID = res.dept_id;
-            }
-          })
-
-          connection.query(
-            'INSERT INTO employee SET ?',
+        inquirer
+          .prompt([
             {
-              first_name: data.first_name,
-              last_name: data.last_name,
-              role_id: roleID
+              name: "first_name",
+              type: "input",
+              message: "Please enter the first name of the new employee: ",
+              validate: function validateInput(name) {
+                return name !== "";
+              },
             },
-            (err) => {
-              if (err) throw err;
+            {
+              name: "last_name",
+              type: "input",
+              message: "Please enter the last name of the new employee: ",
+              validate: function validateInput(name) {
+                return name !== "";
+              },
+            },
+            {
+              name: "choose_role",
+              type: "rawlist",
+              choices() {
+                const choices = [];
+                res.forEach(({ title }) => {
+                  choices.push(title);
+                });
+                return choices;
+              },
+              message: "Choose role for new employee: ",
+            },
+          ])
+          .then((data) => {
+            let roleID;
+            res.forEach((res) => {
+              if (res.title === data.choose_role) {
+                roleID = res.dept_id;
+              }
+            });
 
-              console.log(`New Employee ${data.first_name} ${data.last_name} added!`);
+            connection.query(
+              "INSERT INTO employee SET ?",
+              {
+                first_name: data.first_name,
+                last_name: data.last_name,
+                role_id: roleID,
+              },
+              (err) => {
+                if (err) throw err;
 
-              runSearch();
-            })
+                console.log(
+                  `New Employee ${data.first_name} ${data.last_name} added!`
+                );
 
-        })
+                runSearch();
+              }
+            );
+          });
+      }
+    );
+  };
 
-    });
-  }
+  //Update Employee
 
+  const updateEmpRole = () => {
+    connection.query(
+      `SELECT 
+  e.emp_id,
+  e.first_name,
+  e.last_name,
+  e.manager_id,
+  e.role_id,
+  r.role_id,
+  r.title,
+  r.salary,
+  r.dept_id
+  FROM employee_db.Employee e
+  left join employee_db.role r on e.role_id = r.role_id;`,
+      (err, res) => {
+        if (err) throw err;
+        console.table(res);
 
-
-  
-
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "idUpdate",
+              message: "Please enter the employee id number to update ",
+            },
+            {
+              type: "input",
+              name: "newId",
+              message: "Please enter the updated role id for this emplouyee ",
+            },
+          ])
+          .then((res) => {
+            connection.query(
+              "UPDATE employee SET ? WHERE ?",
+              [
+                {
+                  role_id: res.newId,
+                },
+                {
+                  emp_id: res.idUpdate,
+                },
+              ],
+              (err, res) => {
+                if (err) throw err;
+                console.log(
+                  "The employees role has been updated successfully !!! "
+                );
+                viewAllEmployee();
+              }
+            );
+          });
+      }
+    );
+  };
 };
-
-
-
-
-
